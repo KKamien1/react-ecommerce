8c1ce1c4ea4d74ed1b1a03695b7dfe1b345aeaf4
kamienieckik <kamienieckik@gmail.com>
2019-11-17 18:21:43 +0100

    Product endpoints

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {create, read, remove, update, productById} = require('../controllers/product');
+const {create, read, remove, update, list, listRelated, listCategories, listBySearch, productById, photo} = require('../controllers/product');
 const {requireSignin, isAuth, isAdmin} = require('../controllers/auth');
 const {userById} = require('../controllers/user');
 
@@ -9,8 +9,14 @@ const {userById} = require('../controllers/user');
 router.get('/product/:productId', read);
 router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove );
 router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update );
+router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create)
 
-router.post('/product/create/:userId',requireSignin, isAuth, isAdmin, create)
+router.get('/products', list);
+router.get('/products/related/:productId', listRelated);
+router.get('/products/categories', listCategories);
+
+router.post("/products/by/search", listBySearch);
+router.get("/product/photo/:productId", photo);
 
 router.param('userId', userById);
 router.param('productId', productById)
